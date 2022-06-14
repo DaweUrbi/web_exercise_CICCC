e746a1e6bb33875829000e2d804a523df520cbb9
DaweUrbi <urban.da@icloud.com>
2022-06-14 09:50:56 -0700

    Promise excercise

---
+++ exercise/promises/promises-exercises/03-transforming-promises/answers.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ * EXERCISE 1
+ * 
+ * @param {*} promise 
+ * @param {*} transformer 
+ * @returns {Promise}
+ */
+function mapPromise(promise, transformer){
+  return new Promise((resolve, reject) => {
+    /* IMPLEMENT ME!! */
+  });
+}
+
+/**
+ * 
+ * EXERCISE 2
+ * 
+ * @param {Promise<number | string>} numberPromise 
+ * @returns {Promise<number>}
+ */
+function squarePromise(numberPromise){
+  return numberPromise
+    .then((data) => {
+      if (typeof data === 'number' || Number(data)) {
+        return data * data;
+      } else {
+        throw `Cannot convert ${data} to number`;
+      }
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+/**
+ * EXERCISE 3
+ * 
+ * @param {Promise<number | string>} numberPromise 
+ * @returns {Promise<number>}
+ */
+function squarePromiseOrZero(promise){
+    return squarePromise(promise).catch((error) => {
+        if (error) {
+            return 0;
+        }
+    });
+}
+
+/**
+ * EXERCISE 4
+ * 
+ * @param {Promise} promise 
+ * @returns {Promise}
+ */
+function switcheroo(promise){
+  return promise.then((data) => {
+      throw data;
+  },
+  (error) => {
+      return error;
+  }
+  );
+}
+
+/**
+ * @callback consumer
+ * @param {*} value
+ */
+
+/**
+ * @callback handler
+ * @param {*} error
+ */
+
+module.exports = {
+  mapPromise,
+  squarePromise,
+  squarePromiseOrZero,
+  switcheroo,
+};
---
+++ exercise/promises/promises-exercises/04-chaining-promises/answers.js
@@ -0,0 +1,59 @@
+/**
+ * 
+ * EXERCISE 1
+ * 
+ * @param {Promise} promise 
+ * @param {function} asyncTransformer 
+ */
+function flatMapPromise(promise, asyncTransformer){
+  return new Promise((resolve, reject) => {
+    promise.then((data) => {
+      resolve(asyncTransformer(data));
+    },
+    (error) => {
+      reject(error);
+    }
+    );
+  });
+}
+
+/**
+ * 
+ * EXERCISE 2
+ * 
+ * @param {Promise} firstPromise 
+ * @param {function} slowAsyncProcess 
+ */
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
+  return firstPromise.then((data) => {
+    return slowAsyncProcess(data);
+  });
+}
+
+/**
+ * 
+ * EXERCISE 3
+ * 
+ * @param {function} getUserById 
+ * @param {function} getOrganizationById 
+ */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
+  return function getUserByIdWithOrganization(userId){
+    return getUserById(userId).then((user) => {
+      if (!user) return undefined;
+      return getOrganizationById(user.organizationId).then((organization) => {
+        return { ...user, organization };
+      },
+      () => {
+        return undefined;
+      }
+    );
+  });
+};
+}
+
+module.exports = {
+  flatMapPromise,
+  chainTwoAsyncProcesses,
+  makeGetUserByIdWithOrganization,
+};
