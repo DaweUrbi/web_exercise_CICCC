41c9e11206cc981b1d7813dd9ee224ed22b18303
DaweUrbi <urban.da@icloud.com>
2022-06-15 20:12:48 -0700

    Solution

--- OOP/pizza.js
+++ OOP/pizza.js
@@ -1,5 +1,63 @@
 class Pizza {
-    constructor() {
-      this.toppings = ['cheese'];
-    }
+  constructor(size, type) {
+    this.toppings = ['cheese', 'pizza sauce'];
+    this.size = size;
+    this.type = type;
   }
+  set size(size) {
+    this._size = size;
+  }
+  get size() {
+    return this._size;
+  }
+
+  addToppings(toppings) {
+    return new Promise(
+      (resolve) => {
+        let i = 0;
+        toppings.forEach((topping) => {
+          setTimeout(() => {
+            console.log(`Adding ${topping}....`);
+            this.toppings.push(topping);
+          }, 500 * i);
+          i++;
+        });
+        resolve(i);
+      },
+      (err) => console.log(err)
+    );
+  }
+  bake() {
+    console.log('🍕 on 🔥');
+    setTimeout(() => {
+      console.log('🍕 ready to go!');
+    }, 2000);
+  }
+}
+
+let pizza1 = new Pizza('large', 'veggie');
+// let toppings = ['tomato', 'olives', 'spinach', 'pinapple'];
+// pizza1.addToppings(toppings).then((i) => {
+//   setTimeout(() => {
+//     pizza1.bake();
+//   }, 500 * (i + 1));
+// });
+
+// pizza1.size = 'm';
+// console.log(pizza1.size);
+// pizza1._size = 'l'; // bad practice
+// console.log(pizza1.size);
+
+// const obj = {};
+
+// function createObj() {
+//   const obj = {};
+//   return obj;
+// }
+
+// function Obj() {}
+
+// class Obj {}
+// const obj2 = new Obj();
+
+// const obj3 = new Obj();
--- OOP/toDoList.js
+++ OOP/toDoList.js
@@ -4,28 +4,46 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description, 
+    complete: false,
+    completeTask: function() { 
+      this.complete = true;
+    },
+    logTaskState: function() {
+      console.log(`${title} has${complete ? ' ' : ' not '}been completed`);
+    }
+  };
+  return task;
+//   taskTitles.push(title);
+//   taskComplete.push(false);
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+function completeTask(task) {
+  task.complete = true;
 }
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
+function logTaskState(task) {
+  const title = task.title;
+  const complete = task.complete;
   console.log(`${title} has${complete ? ' ' : ' not '}been completed`);
 }
 
 // DRIVER CODE BELOW
 
-newTask('Clean Cat Litter'); // task 0
-newTask('Do Laundry'); // task 1
+const task1 = newTask(
+  'Clean Cat Litter',
+  'Take all the 💩 out of the litter box'
+);
+const task2 = newTask('Do Laundry', '😨');
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
